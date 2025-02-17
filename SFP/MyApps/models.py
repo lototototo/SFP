c5e6238d554c706b1992e793cd1d083cec400435
lotototo <igneousgun@mail.ru>
2025-02-17 12:25:08 +0300

    Add

--- SFP/MyApps/models.py
+++ SFP/MyApps/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 import datetime
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-class Finding(models.Model):
-    title = models.CharField(max_length=20, blank=False)
-    line_number = models.IntegerField(blank=False)
-    file_path = models.CharField(max_length=20, blank=False)
-    mitigate_before = models.DateTimeField(blank=False)
-    mitigation_violated = models.CharField(max_length=20, blank=False)
+class Scanner(models.Model):
+    title = models.CharField(max_length=20)
+
     def __str__(self):
         return self.title
 
 class Product(models.Model):
-    title = models.CharField(max_length=20, blank=False)
-    business_criticality = models.IntegerField(blank=False)
+    title = models.CharField(max_length=20)
+    business_criticality = models.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(100)])
+
     def __str__(self):
         return self.title
 
-class Scanner(models.Model):
-    title = models.CharField(max_length=20, blank=False)
+class Finding(models.Model):
+    title = models.CharField(max_length=20)
+    line_number = models.IntegerField()
+    file_path = models.CharField(max_length=20)
+    mitigate_before = models.DateField()
+    scanner = models.ForeignKey(Scanner, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    @property
+    def mitigation_violated(self):
+        return self.mitigate_before < datetime.datetime.now()
+
     def __str__(self):
         return self.title
+
+
+
