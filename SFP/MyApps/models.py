7aa8639eaefdfa43b8ca2e7810067975d97aa279
lotototo <igneousgun@mail.ru>
2025-02-19 10:53:04 +0300

    Add

--- SFP/MyApps/models.py
+++ SFP/MyApps/models.py
@@ -22,6 +22,13 @@ class Finding(models.Model):
     mitigate_before = models.DateField()
     scanner = models.ForeignKey(Scanner, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    class Severity_choices(models.IntegerChoices):
+        info = 0
+        low = 1
+        medium = 2
+        high = 3
+        critical = 4
+    severity = models.IntegerField(choices=Severity_choices)
 
     @property
     def mitigation_violated(self):
